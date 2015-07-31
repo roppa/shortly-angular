ca041934c861c909235a82c6e24f534d7ec24e1e
roppa <mark.o.robson@gmail.com>
2015-07-31 16:13:17 -0700

    Passed all spec tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+  $scope.data.links = [];
+
+  $scope.getLinks = function () {
+
+    Links.getLinks()
+      .then(function (links) {
+        $scope.data.links = links;
+        return $scope.data.links;
+      });
+  };
+
+  $scope.getLinks();
 });
